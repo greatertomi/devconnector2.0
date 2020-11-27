1d1eeb762c67f21de321d3a45de016ff1dad9631
Oshalusi John <oshalusijohn@gmail.com>
2020-11-27 18:17:38 +0100

    errors

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const gravatar = require('gravatar');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
+const User = mongoose.model('user');
 
 router.post(
   '/',
@@ -13,12 +18,43 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send({ message: 'Users route' });
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .send({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send({ message: 'User registered' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Server error');
+    }
   }
 );
 
