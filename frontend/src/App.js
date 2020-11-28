d0069412866e0eaf89fc4a72fd0996750fa91db3
Oshalusi John <oshalusijohn@gmail.com>
2020-11-28 21:27:08 +0100

    Began working on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          <Route path='/' exact component={Landing}/>
+          <Route path='/login' exact component={Login}/>
+          <Route path='/register' exact component={Register}/>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
